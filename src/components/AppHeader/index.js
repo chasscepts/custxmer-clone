3f4c50c1e5d55f0057df229ed002ff823c6ae484
Obetta Francis <francischassobetta@gmai.com>
2022-09-13 16:27:39 +0100

    Style Header Component.

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -11,17 +11,25 @@ const AppHeader = () => {
   return (
     <header>
       <h1 className="clip">Custxmer</h1>
-      <div className={css.header}>
-        <Link to="/">
-          <img className={css.logo} alt="logo" src={logo} />
-        </Link>
-        <div className={css.mainMenuWrap}>
-          <MainMenu />
+      <div className={css.headerWrap}>
+        <div className={`container ${css.header}`}>
+          <div className={css.logoWrap}>
+            <Link to="/">
+              <img className={css.logo} alt="logo" src={logo} />
+            </Link>
+          </div>
+          <div className={css.mainMenuWrap}>
+            <MainMenu />
+          </div>
+          <SocialMedia />
         </div>
-        <SocialMedia />
       </div>
       <div className={css.location}>
-        {formatPath(pathname)}
+        <div className={`container ${css.locationInner}`}>
+          <span className={css.locationText}>
+            {formatPath(pathname)}
+          </span>
+        </div>
       </div>
     </header>
   );
